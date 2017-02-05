ca1e7445bc7adb2f8b08d26f55f871ee1c942d3d
cybice <istarkov@gmail.com>
2017-02-06 02:15:26 +0300

    Add readme

---
+++ README.md
@@ -0,0 +1,53 @@
+# GQB
+
+This package allows you to injects type resolvers directly into GraphQL schema language.
+
+## Why
+
+A lot of boilerplate code if write schemas on javascript [graphql-js](https://github.com/graphql/graphql-js).
+
+This is better [graphql-tools](https://github.com/apollographql/graphql-tools) but resolvers described
+after the schema.
+
+## How it works
+
+Using tagged template literals we can place resolvers directly inside schema.
+
+```javascript
+import { gqb, getResolvers } from 'gqb';
+import { graphql } from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const schema = gqb`
+  type Query {
+    hello: String${ // field resolver source
+      () => 'World'
+    },
+    pic(size: Int): String${
+      (_, { size }) => `pic size is: ${size}`
+    }
+  }
+
+  type Mutation {
+    setMessage(message: String): String${ // mutation source
+      (_, { message }) => `Mutated ${message}`
+    }
+  }
+`;
+
+const executableSchema = makeExecutableSchema(
+  getResolvers(schema)
+);
+
+graphql(
+  executableSchema,
+  '{ hello, pic(size: 20)}'
+)
+.then(response => console.log(response));
+```
+
+Type fields resolver now are placed directly in the schema, immediatelly after type!!!
+
+It also supports schema inlining, (*see [tests](./src/__tests__)*)
+
+See the source it's small.
--- src/__tests__/main.spec.js
+++ src/__tests__/main.spec.js
@@ -5,7 +5,7 @@ import { makeExecutableSchema } from 'graphql-tools'; // eslint-disable-line
 test('it works', async () => {
   const schema = gqb`
     type Query {
-      hello: String${
+      hello: String${ // field resolver source
         () => 'World'
       },
       pic(size: Int): String${
@@ -14,10 +14,54 @@ test('it works', async () => {
     }
 
     type Mutation {
+      setMessage(message: String): String${ // mutation source
+        (_, { message }) => `Mutated ${message}`
+      }
+    }
+  `;
+
+  const executableSchema = makeExecutableSchema(
+    getResolvers(schema)
+  );
+
+  const { data } = await graphql(
+    executableSchema,
+    '{ hello, pic(size: 20)}'
+  );
+
+  expect(data).toMatchSnapshot();
+
+  const { data: mutData } = await graphql(
+    executableSchema,
+    'mutation { setMessage(message: "Hello") }'
+  );
+
+  expect(mutData).toMatchSnapshot();
+});
+
+test('it supports schema inlining', async () => {
+  const mutations = gqb`
+    type Mutation {
       setMessage(message: String): String${
         (_, { message }) => `Mutated ${message}`
       }
     }
+  `;
+
+  const queries = gqb`
+    type Query {
+      hello: String${
+        () => 'World'
+      },
+      pic(size: Int): String${
+        (_, { size }) => `pic size is: ${size}`
+      }
+    }
+  `;
+
+  const schema = gqb`
+    ${queries}
+    ${mutations}
   `;
 
   const executableSchema = makeExecutableSchema(
